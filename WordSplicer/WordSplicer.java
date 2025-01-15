c4f8c053fae13977861fa2117f8708107a11c257
Aby Thomas <abythomas300@gmail.com>
2025-01-15 21:53:46 +0530

    Update WordSplicer.java

--- WordSplicer/WordSplicer.java
+++ WordSplicer/WordSplicer.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class WordSplicer{
     public static void main(String args[]){
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter sentence: ");
+            System.out.println("Enter a sentence: ");
             String word=sc.nextLine();
             for(int i=0;i<word.length();i++){
                 System.out.println(word.charAt(i));
